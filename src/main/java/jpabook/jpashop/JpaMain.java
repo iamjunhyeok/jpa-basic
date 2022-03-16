00e5c903725f01a56d3030e35dddab5fc8ebac0d
iamjunhyeok <jeonjhyeok@gmail.com>
2022-03-16 10:42:37 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+
+            em.find(Member.class, "");
+
+//            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+//            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+
+    }
+}
